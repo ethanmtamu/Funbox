1df6be612afdb819b79aad0e73e3218d0b0542b9
Kenny Le <154klee.business@gmail.com>
2025-10-16 17:12:26 -0500

    links

--- GUI/js/communities.js
+++ GUI/js/communities.js
@@ -5,6 +5,9 @@ function initialize() {
     }   
 }
 
+// placeholder
+const baseLink = "communities/"
+
 function createCommunity(community_id) {
     const div = document.createElement("div");
     div.classList.add("community");
@@ -19,7 +22,15 @@ function createCommunity(community_id) {
     div.appendChild(span);
 
     const container = document.querySelector(".communities");
-    container.appendChild(div); 
+    // container.appendChild(div); 
+
+    const link = document.createElement("a");
+    link.href = baseLink + community_id
+    link.appendChild(div);
+
+    container.appendChild(link);
 }
 
 document.addEventListener("DOMContentLoaded", initialize);
+
+// community/community-name/
