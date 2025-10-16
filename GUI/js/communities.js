85e46bf1914207ee0ae2b61b19ef4b3313475616
Roei <roei.benporat@gmail.com>
2025-10-16 17:05:04 -0500

    added image

--- GUI/js/communities.js
+++ GUI/js/communities.js
@@ -3,13 +3,17 @@ function initialize() {
     createCommunity()
 }
 
-function createCommunity() {
-    const div = doucment.createElement("div");
-    div.className = "community";
+function createCommunity(community_id) {
+    const div = document.createElement("div");
+    div.classList.add("community");
+
+    const img = document.createElement("img");
+    img.style.border = "5px solid blue";
 
     const spand = document.createElement("span");
     spand.textContent = "Community name";
 
+    div.appendChild(img);
     div.appendChild(span);
 
     const container = document.querySelector(".communities");
