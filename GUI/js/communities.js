b25334adbd54ee744347356df81789eb43f61171
Kenny Le <154klee.business@gmail.com>
2025-10-16 17:20:55 -0500

    cursor

--- GUI/js/communities.js
+++ GUI/js/communities.js
@@ -8,9 +8,15 @@ function initialize() {
 // placeholder
 const baseLink = "communities/"
 
+function onCommunityClick(community_id) {
+  window.location = baseLink + community_id;
+}
+
 function createCommunity(community_id) {
     const div = document.createElement("div");
     div.classList.add("community");
+    div.onclick = onCommunityClick;
+    div.style.cursor = "pointer";
 
     const img = document.createElement("img");
     img.style.border = "5px solid blue";
@@ -22,13 +28,7 @@ function createCommunity(community_id) {
     div.appendChild(span);
 
     const container = document.querySelector(".communities");
-    // container.appendChild(div); 
-
-    const link = document.createElement("a");
-    link.href = baseLink + community_id
-    link.appendChild(div);
-
-    container.appendChild(link);
+    container.appendChild(div);
 }
 
 document.addEventListener("DOMContentLoaded", initialize);
