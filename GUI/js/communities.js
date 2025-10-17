97acee3887bdc2c4fcb027b72e8189b8dd8b8e48
Kenny Le <154klee.business@gmail.com>
2025-10-17 13:44:41 -0500

    added buttons for creating/joining communities

--- GUI/js/communities.js
+++ GUI/js/communities.js
@@ -1,18 +1,20 @@
+const test_user_data = []
+
 function initialize() {
     console.log("initialize() ran!");
     for (let i = 1; i <= 10; i++) {
-      placeholder = {id: i, name: "F".repeat(i)+"unbox!", memberCount: Math.round(10*i*Math.random())}
-      createCommunity(placeholder);
+      test_user_data[i-1] = {id: i, name: "F".repeat(i)+"unbox!", memberCount: Math.round(10*i*Math.random())}
     }
+    createCommunities(test_user_data);
 }
 
 // placeholder
 const baseLink = "communities/";
 
-function createCommunity(data) {
+function createCommunity(community_data) {
   
     function onCommunityClick() {
-      window.location = baseLink + data.id;
+      window.location = baseLink + community_data.id;
     }
 
     const div = document.createElement("div");
@@ -25,20 +27,37 @@ function createCommunity(data) {
 
     const community_label = document.createElement("span");
     community_label.classList.add("community-name");
-    community_label.innerHTML = data.name;
+    community_label.innerHTML = community_data.name;
 
     const member_label = document.createElement("span");
-    member_label.innerHTML = "<br>"+String(data.memberCount)+" members";
+    member_label.innerHTML = "<br>"+String(community_data.memberCount)+" members!";
 
     const community_infobox = document.createElement("div");
     community_infobox.appendChild(community_label);
     community_infobox.appendChild(member_label);
-
     div.appendChild(img);
     div.appendChild(community_infobox);
 
     const container = document.querySelector(".communities");
     container.appendChild(div);
+}
+
+// TODO: figure out better name for this function
+function createCommunities(user_communities) {
+  user_communities.forEach(community_data => {
+    createCommunity(community_data);
+  });
+  const new_community_button = document.createElement("div");
+  new_community_button.innerHTML = "New community!";
+  new_community_button.classList.add("new-community");
+
+  const find_community_button = document.createElement("div");
+  find_community_button.innerHTML = "Join communities!";
+  find_community_button.classList.add("new-community");
+  
+  // oml
+  const container = document.querySelector(".communities")
+  container.append(new_community_button, find_community_button);
 }
 
 document.addEventListener("DOMContentLoaded", initialize);
